5eef4897acf2fd51fb4df95b6b496ce9831ebf4a
Michal_B <misza1357@gmail.com>
2025-11-26 14:39:32 +0100

    usunięcie niepotrzebnych rzeczy z tekstu

--- src/components/Planner/GardenPlanner.jsx
+++ src/components/Planner/GardenPlanner.jsx
@@ -7,10 +7,6 @@ const GardenPlanner = () => {
       <h2 className="text-[#49733D] mb-8 text-3xl text-center border-b-3 border-[#F2E9BB] pb-4">
         Zaplanuj swój ogród
       </h2>
-      <p className="projekt-intro text-lg leading-loose mb-8 text-center bg-white/70 p-4 rounded-xl shadow-lg">
-        Poznaj szczegóły mojego autorskiego projektu botanicznego, który łączy w sobie pasję do roślin z nowoczesnym podejściem do ich dokumentacji i uprawy. Projekt rozwija się dynamicznie, wzbogacając się o nowe odkrycia i obserwacje.
-      </p>
-      
       <AdvancedGardenPlanner/>
     </div>
   );
